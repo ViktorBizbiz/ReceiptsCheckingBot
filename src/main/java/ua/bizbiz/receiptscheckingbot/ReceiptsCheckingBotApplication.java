4b3fd76bc1f562c27746a862c6dcaefdfbc17676
ViktorBizbiz <bizbizgennadij@gmail.com>
2023-01-29 20:11:26 +0200

    Initial commit

---
+++ src/main/java/ua/bizbiz/receiptscheckingbot/ReceiptsCheckingBotApplication.java
@@ -0,0 +1,13 @@
+package ua.bizbiz.receiptscheckingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptsCheckingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptsCheckingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/bizbiz/receiptscheckingbot/ReceiptsCheckingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.bizbiz.receiptscheckingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptsCheckingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
