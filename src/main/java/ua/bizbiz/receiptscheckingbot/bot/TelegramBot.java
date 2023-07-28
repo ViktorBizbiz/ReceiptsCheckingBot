2e1728477e4a63913883fdfc19b6186911b597a2
ViktorBizbiz <bizbizviktor@gmail.com>
2023-07-28 21:57:26 +0300

    Code refactoring #11

--- src/main/java/ua/bizbiz/receiptscheckingbot/bot/TelegramBot.java
+++ src/main/java/ua/bizbiz/receiptscheckingbot/bot/TelegramBot.java
@@ -2,10 +2,10 @@ package ua.bizbiz.receiptscheckingbot.bot;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.interfaces.Validable;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
@@ -13,23 +13,21 @@ import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ua.bizbiz.receiptscheckingbot.bot.handler.UpdateHandlerFactory;
-import ua.bizbiz.receiptscheckingbot.config.BotConfig;
-import ua.bizbiz.receiptscheckingbot.persistance.entity.Role;
-import ua.bizbiz.receiptscheckingbot.persistance.entity.User;
-import ua.bizbiz.receiptscheckingbot.persistance.repository.UserRepository;
+import ua.bizbiz.receiptscheckingbot.config.bot.BotConfig;
 import ua.bizbiz.receiptscheckingbot.util.DataHolder;
 import ua.bizbiz.receiptscheckingbot.util.PhotoMessageData;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
 
     private final BotConfig config;
-    private final UserRepository userRepository;
     private final UpdateHandlerFactory factory;
     private final DataHolder dataHolder;
 
@@ -78,18 +76,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         dataHolder.setPhotoMessages(photoMessages);
     }
 
-    @SneakyThrows
-    @Scheduled(cron = "0 0 9,16 * * *")
-    private void sendMotivationText() {
-        var users = userRepository.findAllByRoleAndChatIsNotNull(Role.USER);
-        if (users.isPresent()) {
-            for (User user : users.get()) {
-                String motivation = "\uD83D\uDE00 Motivation \uD83D\uDE00";
-                execute(SendMessage.builder()
-                        .text(motivation)
-                        .chatId(user.getChat().getChatId())
-                        .build());
-            }
-        }
+    @Override
+    public <T extends Serializable, Method extends BotApiMethod<T>> T execute(Method method) throws TelegramApiException {
+        return super.execute(method);
     }
 }
--- src/main/java/ua/bizbiz/receiptscheckingbot/config/bot/BotConfig.java
+++ src/main/java/ua/bizbiz/receiptscheckingbot/config/bot/BotConfig.java
@@ -1,14 +1,15 @@
-package ua.bizbiz.receiptscheckingbot.config;
+package ua.bizbiz.receiptscheckingbot.config.bot;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableScheduling
 @Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @PropertySource(value = "application.yaml")
 public class BotConfig {
 
