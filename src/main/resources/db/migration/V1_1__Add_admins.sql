4390be7f46bfda40431c16217ce510702a706fd5
ViktorBizbiz <bizbizgennadij@gmail.com>
2024-01-14 17:01:13 +0200

    Edited README.md

---
+++ src/main/resources/db/migration/V1_1__Add_admins.sql
@@ -0,0 +1,5 @@
+insert into chats (id, chat_id, status)
+values (1, 511002485, 'DEFAULT');
+
+insert into users (id, full_name, role, chat_id, secret_code)
+values (1, 'Bizbiz Viktor', 'ADMIN', 1, 123456);
